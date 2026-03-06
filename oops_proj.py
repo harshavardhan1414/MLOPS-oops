2cddc7a0ceaeee4c2328211740647727e0447e5f
Harshavardhan <pagoluharsha14@gmail.com>
2026-03-06 20:10:24 +0530

    oops overview

--- oops_proj.py
+++ oops_proj.py
@@ -1,9 +1,14 @@
 class chatbook:
     def __init__(self):
+        self.__name="DU"
         self.username=""
         self.password=""
         self.loggedin=False
         self.menu()
+    def get_name(self):
+        return self.__name
+    def set_name(self,value):
+        self.__name=value
     def menu(self):
         user_input=input("""welcome to chatbook ? to preceed? 
         1.press 1 to signup
@@ -66,3 +71,42 @@ class chatbook:
         self.menu()
 
 # res =chatbook()
+#getter and setter method
+# def add(self,*args) method overloading
+
+class animal:
+    #defining constructer
+    def __init__(self,name="Unknown"):
+        self.name=name
+        print("animal constructer called")
+    #instance method
+    def speak(self):
+        print(self.name,"makes a sound")
+    #method overloading sim using *args
+    def info(self,*args):
+        if len(args)==1:
+            print("animal age:",args[0])
+        elif len(args)==2:
+            print("animal age:",args[0],"weight:",args[1])
+        else:
+            print("no add info")
+    #Static method
+    def kingdom():
+        print("all ")
+    def class_info(cls):
+        print("this is animal class")
+class dog(animal):
+    #constructor overriding
+    def __init__(self, name,breed="Unknown"):
+        super().__init__(name)
+        self.breed=breed
+        print("dog con called")
+    #method overriding
+    def speak(self):
+        print(self.name,"barks")
+d1=dog("rock","lab")
+print("metho overlrriding")
+d1.speak()
+print("met overloading")
+d1.info(5)
+d1.info(5,20)
